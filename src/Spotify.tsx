7d9d5f05173e802a005ce8e8ce4f4a74f5db7e61
Adam Stankiewicz <41164873+AdamJStankiewicz@users.noreply.github.com>
2024-03-18 16:30:39 -0400

    dist

--- src/Spotify.tsx
+++ src/Spotify.tsx
@@ -8,6 +8,7 @@ function Spotify(){
     if(breakpoints.md) return <iframe className="spot" src="https://open.spotify.com/embed/artist/6lRAIvgICW7hhoZHsm49hT" width={600} height={400} allowTransparency={true} title="Top Tracks"></iframe>;
     if(breakpoints.sm) return <iframe className="spot" src="https://open.spotify.com/embed/artist/6lRAIvgICW7hhoZHsm49hT" width={500} height={400} allowTransparency={true} title="Top Tracks"></iframe>;
     if(breakpoints.xs) return <iframe className="spot" src="https://open.spotify.com/embed/artist/6lRAIvgICW7hhoZHsm49hT" width={300} height={400} allowTransparency={true} title="Top Tracks"></iframe>;
+    return <iframe className="spot" src="https://open.spotify.com/embed/artist/6lRAIvgICW7hhoZHsm49hT" width={800} height={400} allowTransparency={true} title="Top Tracks"></iframe>
 }
 
 export default Spotify;
